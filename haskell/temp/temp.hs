df352f735b96e2b2aa8c42c412b3d605ecbdf9c6
yorisilo <yorisilo@gmail.com>
2018-02-26 09:53:23 +0900

    update

--- haskell/temp/temp.hs
+++ haskell/temp/temp.hs
@@ -9,3 +9,26 @@ map' f [] = []
 map' f (x:xs) = f x : map' f xs
 
 -- 値を関数へ持ち上げる functor
+actions :: [IO ()]
+actions =
+  [ putStrLn "1"
+  , putStrLn "2"
+  , putStrLn "3"
+  ]
+
+-- main :: IO ()
+-- main = head actions
+
+main :: IO ()
+main = do
+  head actions
+  --sequence_ actions
+
+-- mapM を作ってみるのをやる
+
+ns_sequence :: [IO ()] -> IO()
+ns_sequence [] = do
+  return ()
+ns_sequence (x:xs) = do
+  x
+  ns_sequence xs
